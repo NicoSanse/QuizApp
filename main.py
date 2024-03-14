32e874ceb147d13dcf8edd164ba719d182d1b51b
NicoSanse <n.sansevrino@campus.unimib.it>
2024-03-14 17:40:24 +0100

    .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.secret_key = 'your secret key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Canegrate123'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'login'
 
 mysql = MySQL(app)
