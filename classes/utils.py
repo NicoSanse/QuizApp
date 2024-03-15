a603877b0f183bdc9e26b003642a76787b25d6da
NicoSanse <n.sansevrino@campus.unimib.it>
2024-03-15 18:06:39 +0100

    completed

--- classes/utils.py
+++ classes/utils.py
@@ -2,6 +2,7 @@ import json
 from classes.user import User
 import os
 
+
 def init():
     user = User('ABC', 'ABC', 'abc@gmail.com', 'abc')
     user_dict = user.__dict__
@@ -30,7 +31,63 @@ def add_user(new_user_json):
                     f.write(content)
 
 
-def get_users():
+def update_user(user, score):
     with open('accounts.json', 'r') as f:
         accounts_in_db = json.load(f)
-    return accounts_in_db
+
+    for account in accounts_in_db:
+        if account['id'] == user['id']:
+            account['score'] = score
+
+    with open('accounts.json', 'w') as f:
+        json.dump(accounts_in_db, f)
+
+
+
+
+def get_user_by_email(email):
+    with open('accounts.json', 'r') as f:
+        accounts_in_db = json.load(f)
+
+    for account in accounts_in_db:
+        if account['email'] == email:
+            return account
+
+    return None
+
+def get_users_emails_and_passwords():
+    emails_and_password = []
+    with open('accounts.json', 'r') as f:
+        accounts_in_db = json.load(f)
+
+    for account in accounts_in_db:
+        emails_and_password.append((account['email'], account['password']))
+
+    return emails_and_password
+
+
+def get_user_answers(request):
+    question1 = request.form.get('question1')
+    question2 = request.form.get('question2')
+    question3 = request.form.get('question3')
+    question4 = request.form.get('question4')
+    question5 = request.form.get('question5')
+    question6 = request.form.get('question6')
+    question7 = request.form.get('question7')
+    question8 = request.form.get('question8')
+    question9 = request.form.get('question9')
+    question10 = request.form.get('question10')
+
+    user_answers = []
+    user_answers.append(question1)
+    user_answers.append(question2)
+    user_answers.append(question3)
+    user_answers.append(question4)
+    user_answers.append(question5)
+    user_answers.append(question6)
+    user_answers.append(question7)
+    user_answers.append(question8)
+    user_answers.append(question9)
+    user_answers.append(question10)
+
+    return user_answers
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from classes.user import User
-from classes.utils import init, add_user, get_users
+from classes.utils import init, add_user, get_users_emails_and_passwords, get_user_answers, update_user, get_user_by_email
 import json
 
 app = Flask(__name__)
+# definisco una variabile globale corrispondente alle risposte corrette
+correct_answers = ['A', 'B', 'B', 'A', 'A', 'C', 'B', 'D', 'B', 'C']
 
 ###### 1. Welcome page ######
 @app.route('/')
@@ -41,13 +43,13 @@ def register_post():
             return render_template('register.html', error='Le due password non corrispondono')
         else: 
             # controlliamo se l'utente esiste già
-            if new_user_json in get_users():
+            if new_user_dict['email'] in get_users_emails_and_passwords()[0]:
                 return render_template('register.html', error='L\'utente esiste già')
             # inseriamo l'utente nel db
             else:
                 add_user(new_user_json)
 
-
+    session['user'] = new_user_dict
     return redirect(url_for('index'))
 
 
@@ -66,19 +68,19 @@ def login_post():
     password = request.form['password']
     existing_account = None
 
-    # estraiamo l'utente dal db
-    accounts_in_db = get_users()
+    # estraiamo l'utente dal db come tuple (email, password)
+    accounts_in_db = get_users_emails_and_passwords()
 
     for account in accounts_in_db:
-        if account['email'] == email:
+        if account[0] == email:
             existing_account = account
             break
 
     # verifichiamo l'esistenza dell'utente
     if existing_account:
         # verifichiamo la correttezza della password
-        if existing_account['password'] == password:
-            session['user'] = existing_account
+        if existing_account[1] == password:
+            session['user'] = get_user_by_email(email)
             return redirect(url_for('index'))
         # se la password è sbagliata ritorniamo un errore
         else:
@@ -93,30 +95,66 @@ def login_post():
 @app.route('/index')
 def index():
     if 'user' in session:
-        return render_template('index.html')
+        user = session['user']
+        return render_template('index.html', user=user)
     else:
-        return redirect(url_for('welcome'))
+        return redirect('/')
 
 
 ####### 5. Quiz page ######
 @app.route('/quiz')
 def quiz():
     if 'user' in session:
-        return render_template('quiz.html')
+        user = session['user']
+        return render_template('quiz.html', user=user)
     else:
-        return redirect(url_for('welcome'))
+        return redirect('/')
 
 @app.route('/quiz', methods=['POST'])
 def quiz_post():
-    # to finish
-    return ''
+    if 'user' in session:
+        # otteniamo le risposte dell'utente
+        user_answers = get_user_answers(request)
+        user = session['user']
+
+        print(user_answers)
+        print(correct_answers)
+
+        # otteniamo il punteggio dell'utente
+        user_score = user['score']
+        temp_score = 0
+
+        # calcoliamo il punteggio
+        for i in range(len(user_answers)):
+            if user_answers[i] == None:
+                continue
+            else:
+                if user_answers[i] == correct_answers[i]:
+                    temp_score += 10
+
+        # se il punteggio temporaneo è maggiore del punteggio dell'utente, aggiorniamo il punteggio dell'utente
+        if temp_score > user_score:
+            user_score = temp_score
+            # aggiorniamo l'utente nel db
+            update_user(user, user_score)
+            # aggioriamo l'utente nella sessione
+            user['score'] = user_score
+            session['user'] = user
+
+        return redirect(url_for('index'))
+    
+    else:
+        return redirect('/')
 
 
 ####### 6. Logout page ######
 @app.route('/logout', methods=['POST'])
 def logout():
     session.pop('user', None)
-    return redirect(url_for('welcome'))
+    return redirect('/')
+
+
+
 
 ####### Run app ######
 if __name__ == '__main__':
